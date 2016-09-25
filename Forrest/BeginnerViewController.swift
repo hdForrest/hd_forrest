a240af59045976520fc3827f325801bf45d1c750
이승환 <21400549@handong.edu>
2016-09-25 19:24:19 +0900

    네비게이션 정리 / 초급 까지 코드 연결...

---
+++ Forrest/BeginnerViewController.swift
@@ -0,0 +1,45 @@
+//
+//  BeginnerViewController.swift
+//  Forrest
+//
+//  Created by 이승환 on 2016. 9. 25..
+//  Copyright © 2016년 이승환. All rights reserved.
+//
+
+import UIKit
+
+class BeginnerViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /* 사라질 때 숨기기 */
+    override func viewWillDisappear(animated:Bool)
+    {
+        super.viewWillDisappear(animated)
+        self.navigationController?.navigationBarHidden = true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
