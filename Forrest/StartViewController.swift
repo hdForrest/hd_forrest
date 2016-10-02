ace554c4b91bfb0152bb1eb965414850e01872b8
이승환 <21400549@handong.edu>
2016-10-02 21:14:57 +0900

    사진미션 성공 ! 나머지 버튼도 이미지 입히기 !

--- Forrest/StartViewController.swift
+++ Forrest/StartViewController.swift
@@ -11,11 +11,18 @@ import UIKit
 /* 글자, 배경 색 설정 */
 var background_color_state = 0;
 var text_color_state = 0;
-
+/* 글자 크기 설정 */
 var text_size = 0;
 
 class StartViewController: UIViewController
 {
+    @IBOutlet weak var bt_travel: UIButton!
+    @IBOutlet weak var bt_mission: UIButton!
+    @IBOutlet weak var bt_walk: UIButton!
+    @IBOutlet weak var bt_call: UIButton!
+    @IBOutlet weak var bt_setting: UIButton!
+    
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -27,19 +34,14 @@ class StartViewController: UIViewController
         
         /* 첫화면 배경 색상 설정 */
         self.view.backgroundColor = UIColor(red: CGFloat(100/225.0), green: CGFloat(250/255.0), blue: CGFloat(194/255.0), alpha: CGFloat(1.0))
+        
+        /* 버튼에 이미지 입히기 */
+        
+        bt_mission.setImage(UIImage(named : "Mission"), forState: .Normal)
+        bt_mission.tintColor = UIColor.blackColor()
+        //bt_travel.setTitleColor(UIColor(red: CGFloat(100/225.0), green: CGFloat(250/255.0), blue: CGFloat(194/255.0), alpha: CGFloat(1.0)), forState: .Normal)
+        
     }
-    /*
-    override func viewDidAppear(animated: Bool)
-    {
-        /* 첫화면 네비게이션 바 숨기기. */
-        self.navigationController?.navigationBarHidden = true
-        /* tab bar 숨기기 */
-        self.tabBarController?.tabBar.hidden = true
-        /* 첫화면 배경 색상 설정 */
-        self.view.backgroundColor = UIColor(red: CGFloat(100/225.0), green: CGFloat(250/255.0), blue: CGFloat(194/255.0), alpha: CGFloat(1.0))
-
-    }
-    */
     
     /* 화면 사라질 때 네비게이션 바 나타나기. */
     override func viewWillDisappear(animated:Bool)
@@ -48,14 +50,11 @@ class StartViewController: UIViewController
         self.navigationController?.navigationBarHidden = false
     }
     
-    
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
     
     
     /*
