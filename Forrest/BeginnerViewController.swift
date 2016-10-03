960c447aefe296e4804785496cc1ffa72b2c2e9c
이승환 <21400549@handong.edu>
2016-10-04 00:19:36 +0900

    초급 지도가....안불러와짐 ㅠ.ㅠ

--- Forrest/BeginnerViewController.swift
+++ Forrest/BeginnerViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class BeginnerViewController: UIViewController
 {
+    
     /* 사라질 때 navigation bar 숨기기 */
     override func viewWillDisappear(animated:Bool)
     {
@@ -17,25 +18,26 @@ class BeginnerViewController: UIViewController
         self.navigationController?.navigationBarHidden = true
     }
     
-    /* navigation bar set */
+    
     override func viewWillAppear(animated: Bool)
     {
         super.viewWillAppear(animated)
         
+        /* navigation bar set */
         self.tabBarController?.navigationItem.title = "초급 주행"
         self.tabBarController?.navigationController!.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
         self.tabBarController?.navigationController!.navigationBar.barTintColor = UIColor.blackColor()
         
         self.tabBarController?.navigationController!.navigationBar.tintColor = UIColor(red: CGFloat(100/225.0), green: CGFloat(250/255.0), blue: CGFloat(194/255.0), alpha: CGFloat(1.0))
         
-        /* tab bar set */
-        UITabBarItem.appearance().setTitleTextAttributes([NSFontAttributeName: UIFont(name : "AppleSDGothicNeo-Regular", size: 28)!], forState: .Normal)
         
-        UITabBarItem.appearance().titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -5)
+        /* tab bar set */
+        UITabBarItem.appearance().setTitleTextAttributes([NSFontAttributeName: UIFont(name : "AppleSDGothicNeo-Regular", size: 25)!], forState: .Normal)
+        
+        UITabBarItem.appearance().titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -7)
         
         self.tabBarController?.tabBar.backgroundColor = UIColor(red: CGFloat(54/225.0), green: CGFloat(54/255.0), blue: CGFloat(54/255.0), alpha: CGFloat(1.0))
         self.tabBarController?.tabBar.tintColor = UIColor(red: CGFloat(100/225.0), green: CGFloat(250/255.0), blue: CGFloat(194/255.0), alpha: CGFloat(1.0))
-        
     }
     
     
@@ -44,6 +46,18 @@ class BeginnerViewController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        //imageView = UIImageView(image: UIImage(named: "beginnerMap"))
+        
+        
+        //ScrollView = UIScrollView(frame: view.bounds)
+        //ScrollView.contentSize = imageView.bounds.size
+        //ScrollView.autoresizingMask = [UIViewAutoresizing.FlexibleWidth, UIViewAutoresizing.FlexibleHeight]
+        
+        //ScrollView.addSubview(imageView)
+        //view.addSubview(ScrollView)
+        
+        //self.ScrollView.addSubview()
         // Do any additional setup after loading the view.
     }
 
