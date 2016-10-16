3738fb8f66e44574f808895ebb5ea26c3e08af8d
이승환 <21400549@handong.edu>
2016-10-16 23:53:20 +0900

    자율 주행 파일 만듬

---
+++ Forrest/Free_juhang_ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Free_juhang_ViewController.swift
+//  Forrest
+//
+//  Created by 이승환 on 2016. 10. 16..
+//  Copyright © 2016년 이승환. All rights reserved.
+//
+
+import UIKit
+
+class Free_juhang_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
